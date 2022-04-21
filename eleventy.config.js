cc3f1221a7fd867c13e743c2c5722a25862f0918
Devon Mobley <devon.mobley@gmail.com>
2022-04-21 17:06:25 -0500

    initial commit

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,87 +1,56 @@
-const navigation = require('@11ty/eleventy-navigation')
-const dates = require('./utilities/filters/dates')
-const helpers = require('./utilities/filters/helpers')
-const path = require('path')
+const htmlmin = require('html-minifier');
 
-module.exports = config => {
+module.exports = (eleventyConfig) => {
+  // Add a readable date formatter filter to Nunjucks
+  eleventyConfig.addFilter(
+    'dateDisplay',
+    require('./utilities/filters/dates.js')
+  );
 
-    // navigation plugin
-    config.addPlugin(navigation)
+  // Add a HTML timestamp formatter filter to Nunjucks
+  eleventyConfig.addFilter(
+    'htmlDateDisplay',
+    require('./utilities/filters/timestamp.js')
+  );
 
-    // Human readable date for posts
-    config.addFilter('dateDisplay', dates.friendly)
-
-    // Timestamp for datetime element
-    config.addFilter('timestamp', dates.timestamp)
-
-    // Remove whitespace from a string
-    config.addNunjucksFilter('spaceless', helpers.spaceless)
-
-    // Minify our HTML
-    config.addTransform('htmlminify', require('./utilities/transforms/htmlminify'))
-
-    // Collections
-    config.addCollection('blog', collection => {
-
-        const blogs = collection.getFilteredByTag('blog')
-
-        for ( let i = 0; i < blogs.length; i++ ) {
-
-            const previous_post = blogs[i - 1]
-            const next_post = blogs[i + 1]
-
-            blogs[i].data['previous_post'] = previous_post
-            blogs[i].data['next_post'] = next_post
-
-        }
-
-        return blogs.reverse()
-
-    })
-
-    // Categories collection
-    config.addCollection('categories', collection => {
-
-        const list = new Set()
-
-        collection.getAll().forEach(item => {
-
-            if (!item.data.tags) return
-
-            item.data.tags
-                .filter(category => !['blog', 'all'].includes(category))
-                .forEach(category => list.add(category))
-
-        })
-
-        return Array.from(list).sort()
-
-    })
-
-    // Layout aliases
-    config.addLayoutAlias('base', 'layouts/base.njk')
-    config.addLayoutAlias('home', 'layouts/home.njk')
-    config.addLayoutAlias('page', 'layouts/page.njk')
-    config.addLayoutAlias('blog', 'layouts/blog.njk')
-    config.addLayoutAlias('post', 'layouts/post.njk')
-    config.addLayoutAlias('contact', 'layouts/contact.njk')
-    config.addLayoutAlias('category', 'layouts/category.njk')
-
-    // Include our static assets
-    config.addPassthroughCopy('css')
-    config.addPassthroughCopy('js')
-    config.addPassthroughCopy('images')
-    config.addPassthroughCopy('favicon.png')
-    config.addPassthroughCopy('favicon.svg')
-
-    return {
-        markdownTemplateEngine: 'njk',
-        dir: {
-            input: 'site',
-            output: 'public',
-            includes: 'includes',
-            data: 'globals'
-        }
+  // Minify our HTML
+  eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
+    if (outputPath.endsWith('.html')) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
     }
+    return content;
+  });
 
-}
+  // Collections
+  // eleventyConfig.addCollection('blog', collection => {
+  //     return collection.getFilteredByTag('blog').reverse()
+  // })
+
+  // Layout aliases
+  eleventyConfig.addLayoutAlias('default', 'layouts/default.njk');
+  eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
+
+  // Include our static assets
+  eleventyConfig.addPassthroughCopy('css');
+  eleventyConfig.addPassthroughCopy('javascript');
+  eleventyConfig.addPassthroughCopy('images');
+
+  return {
+    templateFormats: ['md', 'njk'],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    passthroughFileCopy: true,
+
+    dir: {
+      input: 'site',
+      output: 'dist',
+      includes: 'includes',
+      data: 'globals',
+    },
+  };
+};
